24fdce78297f00af60296ab0b0fbe752526a2d70
cassiooliveiraia <cassio@cassiooliveira-ia.com.br>
2025-06-08 23:16:34 -0300

    Update database.py
    fix: adiciona classes Person, Client, Unit e UnitOwner ao models.database

--- src/models/database.py
+++ src/models/database.py
@@ -1,3 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(128), nullable=False)
+
+class Unit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(64), nullable=False)
+
+class UnitOwner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
+    unit_id = db.Column(db.Integer, db.ForeignKey('unit.id'), nullable=False)
